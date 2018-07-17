1fbbd66916140204c6b84650be948b49941fea91
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-17 14:27:53 -0500

    added create and show routes for singers

--- Tuesday/SingersAndSongs/Controllers/singerController.js
+++ Tuesday/SingersAndSongs/Controllers/singerController.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-const singers = require('./Models/singer');
+const Singers = require('./Models/singer');
 
 
+//create
+router.get('/new', (req, res) =>{
+    res.render('new.ejs')
+})
+
+router.post('/', (req, res)=>{
+    Singers.create(req.body, (err, singer)=>{
+        if (err)
+            console.error(err)
+        else {
+            console.log(singer)
+        }
+    });
+});
+
+//read
+router.get('/:id', (req, res)=>{
+    Singers.findById(req.params.id, (err, singer)=>{
+        if(err)
+            console.error(err)
+        else {
+            res.render('show.ejs', {
+                singer: singer
+            })
+        }
+    });
+})
 
 
 module.exports = router;
