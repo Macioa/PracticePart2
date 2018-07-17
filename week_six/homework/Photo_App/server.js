a9390e6f46cb245359d8fb290016313937e730e1
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-16 22:16:46 -0500

    userController connected

--- week_six/homework/Photo_App/server.js
+++ week_six/homework/Photo_App/server.js
@@ -3,4 +3,8 @@ const app = express();
 
 require('./Db/db')
 
+
+const userController = require('./Controllers/userController');
+app.use('/users', userController)
+
 app.listen(3000, () => {console.log('Express server listening')})
