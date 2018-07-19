640a053c052158e633d32b42e8b23188c49d09c1
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-19 13:00:49 -0500

    password compare hash

--- week_six/blog-made-in-class/controllers/auth.js
+++ week_six/blog-made-in-class/controllers/auth.js
@@ -3,6 +3,8 @@ const router  = express.Router();
 const User    = require('../models/users');
 const bcrypt = require('bcrypt');
 
+const chalk = require('chalk')
+
 router.get('/', (req, res) => {
 
   res.render('auth/login.ejs', {
@@ -12,18 +14,31 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
 
   // req.session is availiable on EVERY SINGLE REQUEST FROM THE CLIENT
   // our session is availiable in the following
   console.log(req.session);
 
+
+  //bcrypt.compareSync('plaintext password', hashedPassword)
   // YOU CAN ADD ANY PROPERTY YOU WANT TO THE SESSION,
   // and as soon as you do that its saved to the store
-  req.session.loggedIn = true;
-  req.session.username = req.body.username;
+  var foundUser = await User.find({username:req.body.username}, (err, user)=>{
+    if (user){
+      if (bcrypt.compare(bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)), user.password)){
+        console.log('password match');
+        req.session.loggedIn = true;
+        req.session.username = req.body.username;
+        res.redirect('/articles');
+      } else {
+        console.log('password does not match');
+        req.session.loggedIn = false;
+        res.redirect('/login');
+      }
+    }
+  })
 
-  res.redirect('/articles');
 
 })
 
