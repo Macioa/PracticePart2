288d2d471679f97772bf33220c0a88eee0fa59d8
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-30 13:20:17 -0500

    update state on click

--- week_seven/introReact/react-intro/src/Hello.js
+++ week_seven/introReact/react-intro/src/Hello.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
-  state={user: this.props.user}
+  state={user: this.props.user, count:0}
+  handleClick=(e)=>{
+    this.setState({count: this.state.count+1}); 
+  }
   render(){
     return (
       <div>
           <h1>Hello {this.state.user||'World'}</h1>
+          <button onClick={this.handleClick}>{this.state.count}</button>
           <p>
               <img width='100px' {...this.props}/>
               <img width='200px' src="https://images.pexels.com/photos/1244159/pexels-photo-1244159.jpeg?cs=srgb&dl=4k-wallpaper-blur-close-up-1244159.jpg&fm=jpg" alt = "img from pexels.com"/>
