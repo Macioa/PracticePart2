42e575489fd5a410d6a98f8773994057a407b03d
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-30 17:32:08 -0500

    homework start

---
+++ week_seven/Homework/react-atm-lab-master/src/Account/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Account extends Component {
+  state={balance: 0}
+  render() {
+    return (
+      <div className="account">
+        <h2>{this.props.name}</h2>
+        <div className="balance">{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" />
+        <input type="button" value="Deposit" />
+        <input type="button" value="Withdraw" />
+      </div>
+    )
+  }
+}
+
+export default Account;
