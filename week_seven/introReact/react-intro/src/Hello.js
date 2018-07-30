a34dc5352f00cb3512ced862a42e21b30aa14c8f
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-30 12:59:47 -0500

    override props, dynamic components

--- week_seven/introReact/react-intro/src/Hello.js
+++ week_seven/introReact/react-intro/src/Hello.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
+  state={user: this.props.user}
   render(){
     return (
       <div>
-          <h1>HelloWorld</h1>
+          <h1>Hello {this.state.user||'World'}</h1>
           <p>
+              <img width='100px' {...this.props}/>
               <img width='200px' src="https://images.pexels.com/photos/1244159/pexels-photo-1244159.jpeg?cs=srgb&dl=4k-wallpaper-blur-close-up-1244159.jpg&fm=jpg" alt = "img from pexels.com"/>
           </p>
       </div>
--- week_seven/introReact/react-intro/src/index.js
+++ week_seven/introReact/react-intro/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Hello';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello user='Monica' width="40px" src="https://78.media.tumblr.com/421b9294f4f9505f214f0666c9c8a15b/tumblr_mxwnmkiwuh1t6o9c1o1_500.gif" alt="img from jim"/>, document.getElementById('root'));
 registerServiceWorker();
