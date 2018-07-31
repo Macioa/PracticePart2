220d899659ea85b9022ef9906758360ddb96ab70
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-31 11:11:43 -0500

    tuesday lesson. login form created

---
+++ week_seven/react/forms/src/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render(){
+        return(
+            <form>
+                <input type='text' name='username' placeholder='username'/>
+                <input type='password' name='password' placeholder='password'/>
+                <input type='submit' value='Submit'/>
+            </form>
+        )
+    }
+}
+
+export default Login;
