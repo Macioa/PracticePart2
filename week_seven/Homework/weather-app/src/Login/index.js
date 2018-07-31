995df50df4af65fc2c390de2810cd62622d2d4d4
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-31 17:14:56 -0500

    initialized app, imported login from dogbreed-app

---
+++ week_seven/Homework/weather-app/src/Login/index.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react';
+
+class Login extends Component {
+    state = {}
+    constructor(props){
+        super(props)
+        this.login=this.props.login
+    }
+    handleChange=e=>{
+        this.setState({[e.target.name] : e.target.value.length ? e.target.value:false});
+    }
+    handleSubmit=e=>{
+        e.preventDefault();
+        this.login({auth:this.state.username})
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type='text' name='username' onChange={this.handleChange} value={this.state.username} placeholder='username'/>
+                <input type='password' name='password' onChange={this.handleChange} value={this.state.password} placeholder='password'/>
+                <input type='submit' value='Submit'/>
+            </form>
+        )
+    }
+}
+
+export default Login;
