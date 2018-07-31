700fb86bf21816b4062241fbad9dfc29ed14cddd
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-30 22:03:59 -0500

    better version

--- week_seven/Homework/react-atm-lab-master/src/Account/index.js
+++ week_seven/Homework/react-atm-lab-master/src/Account/index.js
@@ -4,22 +4,22 @@ import React, { Component } from 'react';
 class Account extends Component {
   state={balance: 0}
 
-  handleClick=(e)=>{
-    let value=Math.abs(e.target.form.amount.value*1.0), balance = this.state.balance;
-    if (isNaN(value)) value=0;
-    if (e.target.value!="Deposit") value=-value;
-    this.setState({balance: (balance+value>0)? balance+value:0})
+  handleClick=p=>e=>{
+    let value=(e.target.form.amount.value*1.0).toFixed(2), balance = this.state.balance;
+    this.setState({balance: (balance+p*value>0)? 
+      (parseFloat(Math.round((balance+p*value)*100)).toFixed(2)/100) : 0
+    })
   }
 
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">{this.state.balance}</div>
+        <div className="balance">${this.state.balance}</div>
         <form>
-          <input type="text" placeholder="enter an amount" name="amount"/>
-          <input type="button" value="Deposit" onClick={this.handleClick}/>
-          <input type="button" value="Withdraw" onClick={this.handleClick}/>
+          <input type="number" min="0" step="0.01" placeholder="enter an amount" name="amount"/>
+          <input type="button" value="Deposit" onClick={this.handleClick(1)}/>
+          <input type="button" value="Withdraw" onClick={this.handleClick(-1)}/>
         </form>
       </div>
     )
