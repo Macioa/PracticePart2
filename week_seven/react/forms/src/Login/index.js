2e09faa9d190afb28392e720ea50f28e7e0bbd00
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-31 11:27:47 -0500

    lesson: rerender onChange

--- week_seven/react/forms/src/Login/index.js
+++ week_seven/react/forms/src/Login/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    state={ username:'', password:'' }
+    handleChange=e=>{
+        this.setState({[e.target.name] : e.target.value});
+    }
     render(){
         return(
             <form>
-                <input type='text' name='username' placeholder='username'/>
-                <input type='password' name='password' placeholder='password'/>
+                <input type='text' name='username' onChange={this.handleChange} value={this.state.username} placeholder='username'/>
+                <input type='password' name='password' onChange={this.handleChange} value={this.state.password} placeholder='password'/>
                 <input type='submit' value='Submit'/>
             </form>
         )
