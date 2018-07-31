2c274f8d8e435cc2c5b8c4a93769dc239ebde380
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-31 14:19:50 -0500

    lab start

---
+++ week_seven/react/forms/src/MainContainer/DogBreedForm/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class DogBreedForm extends Component{
+    constructor(props){
+        super(props);
+
+        let {name,age,breed}=this.props;
+        this.state={name:name||'', age:age||'', breed:breed||''};
+    }
+    handleChange=(e)=>{
+        this.setState({[e.target.name]:e.target.value})
+    }
+    handleSubmit=(e)=>{
+        console.log('submit')
+    }
+    render(){
+        return(
+            <div borderstyle='solid'>
+                <form >
+                    <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
+                    <input type="number" name="age" placeholder="Age" value={this.state.age} onChange={this.handleChange}/>
+                    <input type="text" name="breed" placeholder="Breed" value={this.state.breed} onChange={this.handleChange}/>
+                    <br/>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default DogBreedForm;
--- week_seven/react/forms/src/MainContainer/index.js
+++ week_seven/react/forms/src/MainContainer/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import DogBreedForm from './DogBreedForm';
 
 class MainContainer extends Component{
+    state={dogs:[]}
     render(){return(
         <div>
-            Main Container!
+            <h2>Main Container!</h2>
+            <h4>Dogs</h4>
+                {console.log()/*this.state.dogs.map((, )=><)*/}
+                <br/>
+                <DogBreedForm/>
+                
         </div>
     )}
 }
