bdda9dce798b347d5cef534fb7b52a61d4fd96cb
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-31 19:09:16 -0500

    api info

--- week_seven/Homework/weather-app/src/Main/index.js
+++ week_seven/Homework/weather-app/src/Main/index.js
@@ -2,8 +2,24 @@ import React, {Component} from 'react'
 import LocationInput from './Stateful/LocationInput'
 
 class Main extends Component{
+    state={
+        appId: '',
+        reportType: '_5day',
+        connect:{
+            _current:'https://api.openweathermap.org/data/2.5/weather?zip=',
+            _5day:'https://api.openweathermap.org/data/2.5/forecast?zip=',
+            _16day: 'https://api.openweathermap.org/data/2.5/forecast/daily?zip='
+        }
+    }
     logState=()=>console.log(this.state)
     setLocation=location=>this.setState({location:location})
+    getWeather=async(connectionString)=>{
+         try{
+            const weather = await fetch(`${connectionString}${this.state.location.zip},us&APPID=${this.state.appId}`)
+        } catch(err){
+            console.error(err);
+        }
+    }
     render(){
         return(
             <div>
