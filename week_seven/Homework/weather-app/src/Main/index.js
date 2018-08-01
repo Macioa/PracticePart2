b1068b6aacf197c39f6b690fd0262334b205ec9a
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-31 19:51:28 -0500

    receiving weather data

--- week_seven/Homework/weather-app/src/Main/index.js
+++ week_seven/Homework/weather-app/src/Main/index.js
@@ -3,19 +3,26 @@ import LocationInput from './Stateful/LocationInput'
 
 class Main extends Component{
     state={
-        appId: '',
+        appId: require('../appId'),
         reportType: '_5day',
         connect:{
             _current:'https://api.openweathermap.org/data/2.5/weather?zip=',
             _5day:'https://api.openweathermap.org/data/2.5/forecast?zip=',
             _16day: 'https://api.openweathermap.org/data/2.5/forecast/daily?zip='
-        }
+        },
+        report:'none'
     }
     logState=()=>console.log(this.state)
-    setLocation=location=>this.setState({location:location})
-    getWeather=async(connectionString)=>{
+    setLocation=(location)=>{
+        this.getWeather(this.state.connect[this.state.reportType], location.zip).then(
+            weatherData=>this.setState({location:location, report:weatherData})
+        )
+    }    
+    getWeather=async(connectionString,zip)=>{
          try{
-            const weather = await fetch(`${connectionString}${this.state.location.zip},us&APPID=${this.state.appId}`)
+            const weather = await fetch(`${connectionString}${zip},us&APPID=${this.state.appId}`)
+            const parsedWeather = await weather.json();
+            return parsedWeather;
         } catch(err){
             console.error(err);
         }
